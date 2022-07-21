fceae1e1b0e815654b76e67ae4eea2cf6e752250
Nick Kubiak <kubiani@users.noreply.github.com>
2022-07-21 13:37:14 -0500

    Update hello-dag.py

--- hello-dag.py
+++ hello-dag.py
@@ -1,18 +1,12 @@
 from datetime import datetime, timedelta
-from textwrap import dedent
+from pprint import pprint
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-
-def hello_world():
-    print("Hello World!")
-
-# Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import task
 
 with DAG(
-    'hello-world',
+    dag_id='hello-world',
     description='A DAG for Saying Hello',
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2021, 1, 1),
@@ -20,8 +14,12 @@ with DAG(
     tags=['hello','example'],
 ) as dag:
     
-    say_hello = PythonOperator(
-        task_id="hello_world",
-        python_callable=hello_world)
-
-    say_hello
+    @task(task_id="say_hello")
+    def hello_world(ds=None, **kwargs):
+        pprint(kwargs)
+        print(ds)
+        print("Hello World!")
+        
+        return "Howdy"
+        
+    howdy = hello_world()
